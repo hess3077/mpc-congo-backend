87b13f6437991b348c0fd8f777a5f356d36c56fd
uid109483 <uid109483@debian-wheezy-xfs>
2019-02-26 00:06:48 +0100

    resolved conflict

---
+++ public/github-sync.php
@@ -0,0 +1,20 @@
+<?php
+
+$env     = isset($_GET['env']) ? $_GET['env'] : 'frontend';
+$isLocal = isset($_GET['is_local']) ? true : false;
+
+$uri          = 'https://hess3077:T3R2GAvp@github.com/hess3077';
+$dir_frontend = !empty($isLocal) ? '&& cd mpc-congo' : '';
+
+// On lance la synchronisation du serveur de PROD avec le serveur Github
+
+switch ($env) {
+    case 'backend':
+        `cd backend && git pull $uri/mpc-congo-backend/`; 
+    break;
+    default:
+        `cd ../../ $dir_frontend && git pull $uri/mpc-congo/`;
+    break;
+}
+
+
