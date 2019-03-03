51d1f3a46d89aff83330bf229dcab94bd1d6111d
Hess KITOKO - NGOMA <hess.website@gmail.com>
2019-03-03 15:05:36 +0100

    update github sync

--- public/github-sync.php
+++ public/github-sync.php
@@ -6,13 +6,15 @@ $isLocal = isset($_GET['is_local']) ? true : false;
 $uri          = 'https://hess3077:T3R2GAvp@github.com/hess3077';
 $dir_frontend = !empty($isLocal) ? '&& cd mpc-congo' : '';
 
+$cmd_git_pull  = 'git checkout -- . && git pull';
+
 // On lance la synchronisation du serveur de PROD avec le serveur Github
 
 switch ($env) {
     case 'backend':
-        `git pull $uri/mpc-congo-backend/`; 
+        `$cmd_git_pull $uri/mpc-congo-backend/`; 
     break;
     default:
-        `cd ../../ $dir_frontend && git pull $uri/mpc-congo/ && cp dist/mpccongo/* web/`;
+        `cd ../../ $dir_frontend && $cmd_git_pull $uri/mpc-congo/ && cp dist/mpccongo/* web/`;
     break;
 }
